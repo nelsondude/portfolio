7cb82d561edc60378535c137a3e56fcaf25c81ff
nelsondude <alexn1336@gmail.com>
2018-09-06 16:47:52 -0400

    Implement contact button for email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Button} from "components";
 import './Contact.css';
 
 
 const contact = () => (
   <div className={'Contact'}>
-
     <h1>ok, lets create <br/>something great together</h1>
     <p>If you like my work, and are recruiting or want to collaborate, <br/>
       just email me below.</p>
+    <Button clicked={() => document.location.href = "mailto:alexn1336@gmail.com"}>get in touch</Button>
   </div>
 );
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import './index.css';
 
 export const Buffer = (props) => (
   <div style={{height: props.height || '100px', width: '100%'}}/>
+);
+
+export const Button = (props) => (
+  <button className={'Button'} onClick={props.clicked}>
+    {props.children}
+  </button>
 );
