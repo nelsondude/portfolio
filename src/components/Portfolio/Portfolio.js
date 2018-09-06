84b7a027b394ff29e7229f6b5ba1fb87eb4c9431
nelsondude <alexn1336@gmail.com>
2018-09-06 19:23:11 -0400

    Add extra detail to project descriptions

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -12,21 +12,25 @@ const portfolio = () => {
       header: 'SayNPlay',
       link: 'chess.alexnelson.me',
       description: `As a personal project, I created SayNPlay to play real-time
-                    3D chess with other players. There is a chess AI you can play as well`,
+                    3D chess with other players. It includes a chess AI developed with 
+                    the Minimax algorithm in Python.`,
       image: 'chess1.png'
     },
     {
       header: 'Tipsyapp',
       link: 'www.tipsyapp.co',
       description: `Tipsyapp parses and stores all of the “Tipsy Bartender” cocktail
-                    recipes to make it easier to navigate his 1000+ recipes.`,
+                    recipes to make it easier to navigate his 1000+ recipes. Input the 
+                    bar ingredients you have at home to see what drinks you can make of his.`,
       image: 'tipsy1.png'
     },
     {
       header: 'BamSEC',
       link: 'www.bamsec.com',
       description: `In the summer of 2018, I interned at BamSEC, which is a 
-                    financial research tool for SEC filings posted on SEC.gov`,
+                    financial research tool for SEC filings posted on SEC.gov. I worked on
+                    a user interaction monitoring system and I optimized our elastic search
+                    indexing with AWS.`,
       image: 'bamsec1.png'
     }
   ];
