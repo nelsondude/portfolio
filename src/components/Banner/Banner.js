9d1319b2ebe48cb79ade43df40a43608b5a86e54
nelsondude <alexn1336@gmail.com>
2018-12-17 17:43:34 -0500

    Add particles js to the landing page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,10 +3,29 @@ import {Buffer} from "../index";
 import './Banner.css';
 import Navbar from "../Navbar/Navbar";
 import SocialIcons from "../SocialIcons/SocialIcons";
+import Particles from "react-particles-js";
 
 
 const banner = () => (
   <div className={'Banner'}>
+    <Particles className={'particles-wrapper'} params={{
+      "particles": {
+        "number": {
+          "value": window.innerWidth < 400 ? 25 : 50
+        },
+        "size": {
+          "value": 3
+        }
+      },
+      "interactivity": {
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "grab"
+          }
+        }
+      }
+    }}/>
     <div className="banner-content">
       <h1 className="text-center banner-title">Alexander Nelson</h1>
       <Buffer height={'40px'}/>
