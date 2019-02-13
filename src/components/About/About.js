cafc499dfcc8ab0c22d2eff6a281c06b60087403
nelsondude <alexn1336@gmail.com>
2019-02-13 14:38:47 -0500

    Updated about section with relevant information

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,13 +14,18 @@ const about = () => {
         <h1>About Me</h1>
         <br/>
         <p>
-          Currently a Senior in Information Systems at Carnegie Mellon University with an
-          expected graduation of June 2019, I’m also a Software Engineer with 2+ years of
+          My name is Alex Nelson and I'm currently a Senior in Information Systems at Carnegie Mellon University with an
+          expected graduation of May 2019. I love to code and have 2+ years of Software Engineering
           experience specializing in Full-Stack web development. <br/><br/>
 
-          I’ve held Software Engineering internships with BamSEC, The Walt Disney Company,
-          and EVVEMI Inc. doing mostly Full-Stack web development. I’m currently looking
-          for a full-time position following my graduation next summer. <br/><br/>
+          I’ve held Software Engineering internships with <a href="https://www.bamsec.com/">BamSEC</a>, <a href="https://www.thewaltdisneycompany.com/">The Walt Disney Company</a>,
+          and <a href="https://www.evvemi.com/">EVVEMI Inc.</a> doing Software Engineering in a variety of fields. I plan on working at
+          <a href="https://www.compass.com/">Compass Real Estate</a> as a Software Engineer
+          following my graduation in May 2019. <br/><br/>
+
+          I love using new technologies like React, Gulp.js, Django and more! These are just a few of my favorites
+          for getting a project up and running. I've recently started deploying sites on Firebase for static content
+          and Heroku when I need a more robust backend. <br/><br/>
 
           In my free time, I love to travel, play chess, exercise, and hangout with
           friends and family.
