e65b9a93214177a605dca8f094ec99664f2a07cb
nelsondude <alexn1336@gmail.com>
2019-03-29 18:30:21 -0400

    Reset animation with a click

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,22 +4,32 @@ import './Banner.css';
 import Navbar from "../Navbar/Navbar";
 import SocialIcons from "../SocialIcons/SocialIcons";
 import {ReactComponent as Signature} from './signature.svg';
-import Particles from "react-particles-js";
 
 
-const banner = () => (
-  <div className={'Banner'}>
-    <div className="banner-content">
-      <Signature style={{padding: "5px"}}/>
-      <Buffer height={'40px'}/>
-      <SocialIcons />
-      <Buffer height={'80px'}/>
-      <h3>I’m a Software Engineer and Full-Stack web developer focused on
-        creating brilliant user-experiences and robust code.</h3>
-    </div>
+class Banner extends React.Component {
+  resetAnimation = () => {
+    var el = document.getElementById('animated');
+    var newone = el.cloneNode(true);
+    newone.onclick = this.resetAnimation;
+    el.parentNode.replaceChild(newone, el);
+  };
 
-    <Navbar />
-  </div>
-);
+  render() {
+    return (
+      <div className={'Banner'}>
+        <div className="banner-content">
+          <Signature style={{padding: "5px"}} onClick={this.resetAnimation} id={"animated"}/>
+          <Buffer height={'40px'}/>
+          <SocialIcons />
+          <Buffer height={'80px'}/>
+          <h3>I’m a Software Engineer and Full-Stack web developer focused on
+            creating brilliant user-experiences and robust code.</h3>
+        </div>
 
-export default banner;
+        <Navbar />
+      </div>
+    )
+  }
+}
+
+export default Banner;
